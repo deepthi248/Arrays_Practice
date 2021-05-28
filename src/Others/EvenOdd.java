bd44f41c2c38120ba398c8f59520e194c32f4bee
deepthi <deepthibhogaraju.3@gmail.com>
2021-05-28 12:08:23 +0530

    bring all even numbers to first part and odd numbers to second

---
+++ src/Others/EvenOdd.java
@@ -0,0 +1,38 @@
+package Others;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int arr[] ={90,87,45,64};
+        evenOddArrangement(arr);
+        for (int value:arr)
+            System.out.print(value+" ");
+
+        }
+    public static void evenOddArrangement(int[] arr){
+        int mid = 0+(arr.length-1 -0)/2;
+        int even_counter=0, odd_counter= arr.length-1;
+        for( int i=0;i <= mid;){
+            if( arr[i]%2 !=0 && odd_counter > i){ //exchange
+                if(arr[odd_counter] % 2 == 0){
+                    swap(arr, odd_counter,i);
+                    odd_counter--;
+                    i++;
+                }
+                else if( odd_counter > i){
+                    odd_counter--;
+                    swap(arr, odd_counter,i);
+                    odd_counter--;
+
+                }
+            }
+
+            else
+                i++;
+        }
+    }
+    public static void swap(int [] arr, int j , int i){
+        int temp=arr[i];
+        arr[i] =arr[j];
+        arr[j]=temp;
+    }
+}
